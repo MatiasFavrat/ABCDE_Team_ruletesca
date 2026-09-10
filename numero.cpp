a693801d05ce31cbeb083677afd77f0c5d29fffb
matu <158612424+MatiasFavrat@users.noreply.github.com>
2026-09-10 17:34:24 +0000

    Modificacion en la funcion ObtenerValor

--- numero.cpp
+++ numero.cpp
@@ -3,56 +3,57 @@
 
 /**
 * ****************************************************************************************
-* Función: inicializarNumero
+* Funciï¿½n: inicializarNumero
 *
-* Parámetros:
-* - Numero &n : Referencia al objeto Numero que será inicializado.
-* - int valor : Valor numérico que se asignará al objeto.
-* - char color : Carácter que representa el color del número.
+* Parï¿½metros:
+* - Numero &n : Referencia al objeto Numero que serï¿½ inicializado.
+* - int valor : Valor numï¿½rico que se asignarï¿½ al objeto.
+* - char color : Carï¿½cter que representa el color del nï¿½mero.
 *
 * Retorna:
-* - void : No retorna ningún valor.
+* - void : No retorna ningï¿½n valor.
 *
-* Descripción:
+* Descripciï¿½n:
 * Inicializa los atributos del parameto Numero con los valores recibidos y valida
 * la consistencia de los datos ingresados.
 * ****************************************************************************************
 */
 
 void inicializarNumero(Numero &n, int valor, char color){
-	return 0;
+
 }
 
 /**
 * ****************************************************************************************
-* Función: obtenerValor
+* Funciï¿½n: obtenerValor
 *
-* Parámetros:
-* - Numero n : Objeto Numero del cual se desea obtener el valor numérico.
+* Parï¿½metros:
+* - Numero n : Objeto Numero del cual se desea obtener el valor numï¿½rico.
 *
 * Retorna:
-* - int : Valor numérico almacenado en el objeto Numero.
+* - int : Valor numï¿½rico almacenado en el objeto Numero.
 *
-* Descripción:
-* Accede al atributo que contiene el valor numérico del objeto y lo retorna.
+* Descripciï¿½n:
+* Accede al atributo que contiene el valor numï¿½rico del objeto y lo retorna.
 * ****************************************************************************************
 */
 
-int obtenerValor(Numero n){ 
-	return 0; 
+int obtenerValor(Numero n){
+	int Valor=n.valor; 
+	return Valor; 
 }
 	
 /**
 * ****************************************************************************************
-* Función: obtenerColor
+* Funciï¿½n: obtenerColor
 *
-* Parámetros:
+* Parï¿½metros:
 * - Numero n : Objeto Numero del cual se desea obtener el color.
 *
 * Retorna:
-* - char : Carácter que representa el color almacenado en el objeto Numero.
+* - char : Carï¿½cter que representa el color almacenado en el objeto Numero.
 *
-* Descripción:
+* Descripciï¿½n:
 * Accede al atributo que contiene el color del objeto y lo retorna.
 * ****************************************************************************************
 */
@@ -72,18 +73,18 @@ char obtenerColor(Numero n){
 }
 /**
 * ****************************************************************************************
-* Función: obtenerParidad
+* Funciï¿½n: obtenerParidad
 *
-* Parámetros:
-* - Numero n : Objeto Numero cuyo valor numérico será analizado.
+* Parï¿½metros:
+* - Numero n : Objeto Numero cuyo valor numï¿½rico serï¿½ analizado.
 *
 * Retorna:
 * - int : 0 si el valor no es par ni impar, 1 si es par y 2 si es impar.
 *
-* Descripción:
-* Analiza el valor numérico almacenado en el objeto y determina su paridad,
+* Descripciï¿½n:
+* Analiza el valor numï¿½rico almacenado en el objeto y determina su paridad,
 * retornando 1 para valores pares, 2 para valores impares y 0 cuando el valor
-* no corresponde a una de estas categorías.
+* no corresponde a una de estas categorï¿½as.
 * ****************************************************************************************
 */
 int obtenerParidad(Numero n){
