233de3cce798da3eefe9081a6bdd530bf07ace44
matu <158612424+MatiasFavrat@users.noreply.github.com>
2026-09-24 16:35:48 +0000

    Funcion MenuPrincipal

--- main.cpp
+++ main.cpp
@@ -2,7 +2,45 @@
 #include "numero.h"
 #include "ruleta.h"
 #include "jugador.h"
+//#include "MenuJuego.h"
+
 using namespace std;
+
+void IniciarNSesion(){
+	cargarJugadores(jugadores[],cant);
+}
+
+void MenuPrincioal(){
+	char op;
+	do{
+		cout << "La Ruletesca\n==========================\n1.- Iniciar nueva sesión de ruleta\n2.- Consultar estado de jugadores\n3.- Mostrar historial de giros\n4.- Mostrar estadísticas de la sesión\n5.- Ordenar sesiones según cantidad de giros (Funcionalidad en Desarrollo)\n6.- Analizar sesiones históricas (Funcionalidad en Desarrollo)\n7.- Carga de Archivo (Funcionalidad en Desarrollo)\nX.- Salir de la aplicación\nIngrese una opción: "
+		cin >> op;
+		swicht(op){
+			case '1':
+				IniciarNSesion();
+				break;
+			case '2':
+				ConsultarEstado();
+				break;
+			case '3':
+				MostratHistorial();
+				break;
+			case '4':
+				MostrarEstadistaSesion();
+				break;
+			case '5':
+				//OrdSesionGiros();
+				break;
+			case '6':
+				//AnalizarSesionHistoricas();
+				break;
+			case '7':
+				//CargarArchivo();
+				break;
+		}
+	}while(op=='x')
+}
+
 
 int main() {
 	
